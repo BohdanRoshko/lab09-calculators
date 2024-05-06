17f697b31d7e4310a4c272f80e9c02f4bc74a38b
Bohdan Roshko <roshko.boh@gmail.com>
2024-05-06 14:56:35 +0300

    add initial migration and changes to styling

---
+++ Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Operation> Operations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Operation>().HasKey(op => op.Id); // Assuming 'Name' is unique and can be used as a key
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- Data/IDataAccess.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Data;
-
-public interface IDataAccess
-{
-    Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
-    Task SaveData<T>(string sql, T parameters, string connectionString);
-}
